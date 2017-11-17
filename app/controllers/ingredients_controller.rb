250d8619b58ba31ab66eca943735b8f079e3780e
Raghu Betina <raghu@firstdraft.com>
2017-11-17 21:36:14 +0000

    Only ingredient user can modify ingredient

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,4 +1,14 @@
 class IngredientsController < ApplicationController
+  before_action :current_user_must_be_ingredient_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_ingredient_user
+    ingredient = Ingredient.find(params[:id])
+
+    unless current_user == ingredient.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @ingredients = Ingredient.all
 
