41246d68ae2801a3ce3b728cffd32476c8a774f6
Raghu Betina <raghu@firstdraft.com>
2017-11-17 21:34:50 +0000

    Generate ingredients resource with starter:resource

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,81 @@
+class IngredientsController < ApplicationController
+  def index
+    @ingredients = Ingredient.all
+
+    render("ingredients/index.html.erb")
+  end
+
+  def show
+    @ingredient = Ingredient.find(params[:id])
+
+    render("ingredients/show.html.erb")
+  end
+
+  def new
+    @ingredient = Ingredient.new
+
+    render("ingredients/new.html.erb")
+  end
+
+  def create
+    @ingredient = Ingredient.new
+
+    @ingredient.user_id = params[:user_id]
+    @ingredient.food_group = params[:food_group]
+
+    save_status = @ingredient.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/ingredients/new", "/create_ingredient"
+        redirect_to("/ingredients")
+      else
+        redirect_back(:fallback_location => "/", :notice => "Ingredient created successfully.")
+      end
+    else
+      render("ingredients/new.html.erb")
+    end
+  end
+
+  def edit
+    @ingredient = Ingredient.find(params[:id])
+
+    render("ingredients/edit.html.erb")
+  end
+
+  def update
+    @ingredient = Ingredient.find(params[:id])
+
+    @ingredient.user_id = params[:user_id]
+    @ingredient.food_group = params[:food_group]
+
+    save_status = @ingredient.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/ingredients/#{@ingredient.id}/edit", "/update_ingredient"
+        redirect_to("/ingredients/#{@ingredient.id}", :notice => "Ingredient updated successfully.")
+      else
+        redirect_back(:fallback_location => "/", :notice => "Ingredient updated successfully.")
+      end
+    else
+      render("ingredients/edit.html.erb")
+    end
+  end
+
+  def destroy
+    @ingredient = Ingredient.find(params[:id])
+
+    @ingredient.destroy
+
+    if URI(request.referer).path == "/ingredients/#{@ingredient.id}"
+      redirect_to("/", :notice => "Ingredient deleted.")
+    else
+      redirect_back(:fallback_location => "/", :notice => "Ingredient deleted.")
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  # Routes for the Ingredient resource:
+  # CREATE
+  get "/ingredients/new", :controller => "ingredients", :action => "new"
+  post "/create_ingredient", :controller => "ingredients", :action => "create"
+
+  # READ
+  get "/ingredients", :controller => "ingredients", :action => "index"
+  get "/ingredients/:id", :controller => "ingredients", :action => "show"
+
+  # UPDATE
+  get "/ingredients/:id/edit", :controller => "ingredients", :action => "edit"
+  post "/update_ingredient/:id", :controller => "ingredients", :action => "update"
+
+  # DELETE
+  get "/delete_ingredient/:id", :controller => "ingredients", :action => "destroy"
+  #------------------------------
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
